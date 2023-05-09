b2f96b9856c7b2a4cce17c547b459b8ce63452b0
umtalj0ta <121719869+umtalj0ta@users.noreply.github.com>
2023-05-09 21:21:05 +0000

    new version

---
+++ get_next_line.c
@@ -0,0 +1,88 @@
+#include "get_next_line.h"
+
+static char	*ft_line(int fd, char *buf, char *backup)
+{
+	int		read_bytes;
+	char	*char_temp;
+
+	read_bytes = 1;
+	while (read_bytes != 0)
+	{
+		read_bytes = read(fd, buf, BUFFER_SIZE);
+		if (read_bytes == -1)
+			return (NULL);
+		else if (read_bytes == 0)
+			break ;
+		buf[read_bytes] = '\0';
+		if (!backup)
+			backup = ft_strdup("");
+		char_temp = backup;
+		backup = ft_strjoin(char_temp, buf);
+		free(char_temp);
+		char_temp = NULL;
+		if (ft_strchr(buf, '\n'))
+			break ;
+	}
+	return (backup);
+}
+
+static char	*refresh_buffer(char *line)
+{
+	size_t	count;
+	char	*backup;
+
+	count = 0;
+	while (line[count] != '\n' && line[count] != '\0')
+		count++;
+	if (line[count] == '\0' || line[1] == '\0')
+		return (0);
+	backup = ft_substr(line, count + 1, ft_strlen(line) - count);
+	if (*backup == '\0')
+	{
+		free(backup);
+		backup = NULL;
+	}
+	line[count + 1] = '\0';
+	return (backup);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	char		*buf;
+	static char	*backup;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buf)
+		return (NULL);
+	line = ft_line(fd, buf, backup);
+	free(buf);
+	buf = NULL;
+	if (!line)
+		return (NULL);
+	backup = refresh_buffer(line);
+	return (line);
+}
+
+// int	main(void)
+// {
+// 	int fd;
+// 	char *line;
+
+// 	fd = open("test.txt", O_RDONLY);
+// 	line = get_next_line(fd);
+// 	printf("%s", line);
+// 	line = get_next_line(fd);
+// 	printf("%s", line);
+// 	line = get_next_line(fd);
+// 	printf("%s", line);
+// 	line = get_next_line(fd);
+// 	printf("%s", line);
+// 	line = get_next_line(fd);
+// 	printf("%s", line);
+
+// 	close(fd);
+// 	return (0);
+// }
---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFFER_SIZE 500
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <stdlib.h>
+#include <stdio.h>
+
+
+
+
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+int		ft_strlen(const char *str);
+char	*ft_strchr(const char *s, int i);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*get_next_line(int fd);
+
+#endif
